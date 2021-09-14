621950a68b6cbee83f0b9cc394de2e44311a9011
Riku Oja <riku.oja@kapsi.fi>
2021-09-14 16:12:43 +0300

    Disable devtools and checks; kepler state is so big that they crash

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -3,17 +3,11 @@ import {enhanceReduxMiddleware} from 'kepler.gl/middleware';
 import scdiReducer from './reducer';
 
 export const store = configureStore({
+  devTools: false,
   reducer: scdiReducer,
   middleware: (getDefaultMiddleware) => 
     enhanceReduxMiddleware(getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [
-          '@@kepler.gl/MOUSE_MOVE',
-          '@@kepler.gl/TOGGLE_MODAL',
-          '@@kepler.gl/UPDATE_MAP',
-          '@@kepler.gl/LOAD_MAP_STYLES',
-          '@@kepler.gl/LAYER_HOVER'
-        ],
-      },
+      serializableCheck: false,
+      immutableCheck: false
     }))
 });
